d139a5ca43e1eaa45eb5481c541cc52172a2904f
Nguyen Thanh Nam <nam@leansoft-inc.com>
2021-10-16 13:29:59 +0700

    [Update] small

--- src/screens/account/Login.js
+++ src/screens/account/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {TextInput} from 'react-native';
-import {View, Button} from 'react-native-ui-lib';
+import {TextInput, TouchableOpacity, ImageBackground} from 'react-native';
+import {View, Button, Text} from 'react-native-ui-lib';
 import {StyleSheet} from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
@@ -36,56 +36,68 @@ export default class Login extends Component {
         const {navigation} = this.props;
 
         return (
+            <ImageBackground source={require('../../images/background.jpeg')} resizeMode="cover" style={{flex: 1, height: UStyle.deviceHeight}}>
 
-            <KeyboardAwareScrollView>
-                <View style={{height: topSpace, width: UStyle.deviceWidth}}/>
+                <KeyboardAwareScrollView>
+                    <View style={{height: topSpace, width: UStyle.deviceWidth}}/>
 
-                <View style={[styles.container, {paddingHorizontal: 20}]} onLayout={this.measureComponentHeight}>
+                    <View style={[styles.container, {paddingHorizontal: 20}]} onLayout={this.measureComponentHeight}>
 
-                    <FormLogin name={'people-sharp'}>
-                        <TextInput
-                            style={{flex: 1, fontSize: 16}}
-                            placeholder='Enter...'
-                            placeholderTextColor='#6B7B8B'
-                            textContentType={'emailAddress'}
-                            keyboardType={'default'}
-                            returnKeyType={'next'}
-                            value={username}
-                            onChangeText={username => this.setState({username})}
-                            onSubmitEditing={() => {
-                                this.passwordTextInput.focus();
-                            }}
+                        <FormLogin iconName={'people-sharp'}>
+                            <TextInput
+                                style={{flex: 1, fontSize: 16}}
+                                placeholder='Enter...'
+                                placeholderTextColor='#6B7B8B'
+                                textContentType={'emailAddress'}
+                                keyboardType={'default'}
+                                returnKeyType={'next'}
+                                value={username}
+                                onChangeText={username => this.setState({username})}
+                                onSubmitEditing={() => {
+                                    this.passwordTextInput.focus();
+                                }}
+                            />
+                        </FormLogin>
+
+                        <FormLogin iconName={'key-sharp'} top>
+                            <TextInput
+                                style={{flex: 1, fontSize: 16}}
+                                ref={(input) => {
+                                    this.passwordTextInput = input;
+                                }}
+                                placeholder='&#8226;&#8226;&#8226;&#8226;&#8226;'
+                                placeholderTextColor='#6B7B8B'
+                                textContentType={'password'}
+                                returnKeyType={'done'}
+                                value={password}
+                                secureTextEntry={hidePassword}
+                                onChangeText={password => this.setState({password})}
+                                onSubmitEditing={() => navigation.navigate('Root')}
+                            />
+                        </FormLogin>
+
+                        <Button backgroundColor={"rgba(48, 182, 80, 0.8)"}
+                                label="SIGN IN"
+                                labelStyle={{fontWeight: '600'}}
+                                style={{marginBottom: ButtonSpace, marginTop: 40, height: 50, borderRadius: 4}}
+                                onPress={() => navigation.navigate('Root')}
+                                fullWidth
                         />
-                    </FormLogin>
 
-                    <FormLogin name='key-sharp' top>
-                        <TextInput
-                            style={{flex: 1, fontSize: 16}}
-                            ref={(input) => {
-                                this.passwordTextInput = input;
-                            }}
-                            placeholder='&#8226;&#8226;&#8226;&#8226;&#8226;'
-                            placeholderTextColor='#6B7B8B'
-                            textContentType={'password'}
-                            returnKeyType={'done'}
-                            value={password}
-                            secureTextEntry={hidePassword}
-                            onChangeText={password => this.setState({password})}
-                            onSubmitEditing={() => navigation.navigate('Root')}
-                        />
-                    </FormLogin>
+                        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20}}>
+                            <TouchableOpacity activeOpacity={0.8} onPress={() => {
+                            }} style={{padding:5, backgroundColor:'rgba(255,255,255,0.9)', borderRadius:4}} >
+                                <Ionicons name={'logo-google'} size={30} color={'#4267B2'}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.8} onPress={() => {
+                            }} style={{marginLeft: 10, padding:5, backgroundColor:'rgba(255,255,255,0.9)', borderRadius:4}}>
+                                <Ionicons name={'logo-facebook'} size={30} color={'#4267B2'}/>
+                            </TouchableOpacity>
+                        </View>
 
-                    <Button backgroundColor="#30B650"
-                            label="SIGN IN"
-                            labelStyle={{fontWeight: '600'}}
-                            style={{marginBottom: ButtonSpace, marginTop: 40, height: 50, borderRadius: 4}}
-                            enableShadow
-                            onPress={() => navigation.navigate('Root')}
-                            fullWidth
-                    />
-
-                </View>
-            </KeyboardAwareScrollView>
+                    </View>
+                </KeyboardAwareScrollView>
+            </ImageBackground>
         );
     }
 }
@@ -93,26 +105,24 @@ export default class Login extends Component {
 /**
  *
  * @param children
- * @param name
- * @param color
+ * @param iconName
+ * @param iconColor
  * @param top
  * @constructor
  */
-const FormLogin = ({children, name = '', color, top}) => {
+const FormLogin = ({children, iconName = '', iconColor, top}) => {
     return (
         <View style={{
             height: 50,
             flexDirection: 'row',
             alignItems: 'center',
             marginTop: top ? 20 : 0,
-            borderColor: '#6B7B8B',
-            borderWidth: 1,
             borderRadius: 4,
             backgroundColor: '#FFF',
             paddingHorizontal: 15,
             opacity: 0.7
         }}>
-            <Ionicons name={name} size={24} color={color} style={{marginRight: 5}}/>
+            <Ionicons name={iconName} size={24} color={iconColor} style={{marginRight: 5}}/>
             {children}
         </View>
     );
