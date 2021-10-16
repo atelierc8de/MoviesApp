04bdd234bfb99218bbaef2e0692ac36fa812316d
Nguyen Thanh Nam <nam@leansoft-inc.com>
2021-10-16 13:31:49 +0700

    [Update] small

--- src/screens/account/Login.js
+++ src/screens/account/Login.js
@@ -39,7 +39,7 @@ export default class Login extends Component {
             <ImageBackground source={require('../../images/background.jpeg')} resizeMode="cover" style={{flex: 1, height: UStyle.deviceHeight}}>
 
                 <KeyboardAwareScrollView>
-                    <View style={{height: topSpace, width: UStyle.deviceWidth}}/>
+                    <View style={{height: topSpace+20+20, width: UStyle.deviceWidth}}/>
 
                     <View style={[styles.container, {paddingHorizontal: 20}]} onLayout={this.measureComponentHeight}>
 
@@ -79,12 +79,12 @@ export default class Login extends Component {
                         <Button backgroundColor={"rgba(48, 182, 80, 0.8)"}
                                 label="SIGN IN"
                                 labelStyle={{fontWeight: '600'}}
-                                style={{marginBottom: ButtonSpace, marginTop: 40, height: 50, borderRadius: 4}}
+                                style={{marginTop: 40, height: 50, borderRadius: 4}}
                                 onPress={() => navigation.navigate('Root')}
                                 fullWidth
                         />
 
-                        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20}}>
+                        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 40}}>
                             <TouchableOpacity activeOpacity={0.8} onPress={() => {
                             }} style={{padding:5, backgroundColor:'rgba(255,255,255,0.9)', borderRadius:4}} >
                                 <Ionicons name={'logo-google'} size={30} color={'#4267B2'}/>
